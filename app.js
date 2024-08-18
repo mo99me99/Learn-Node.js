1788225e82a21232d3a7285e87a5bede8e7f35c0
mo99me99 <m.m.hosseini1099@gmail.com>
2024-08-18 11:46:04 +0330

    011 Parsing Request Bodies

--- app.js
+++ app.js
@@ -1,9 +1,40 @@
 const http = require("http");
+const fs = require("fs");
 
 function rqListener(req, res) {}
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<hmtl></html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      console.log(parsedBody);
+      const file = fs.writeFileSync("./message.txt", message);
+    });
+
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
   res.setHeader("Content-Type", "text/html");
   res.write("<hmtl></html>");
   res.write("<head><title>My First Node Page</title></head>");
