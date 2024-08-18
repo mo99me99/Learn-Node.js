bc43473f478bb0ed94c5ed34c014d880d195b480
mo99me99 <m.m.hosseini1099@gmail.com>
2024-08-18 17:51:49 +0330

    010 Using Express Router

--- app.js
+++ app.js
@@ -1,32 +1,13 @@
 const http = require("http");
 const bodyParser = require('body-parser');
-
 const express = require("express");
 
+const amdinRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:false}))
-
-app.use("/", (req, res, next) => {
-  next();
-});
-
-app.use("/add-product", (req, res, next) => {
-  res.send(`
-    <form action="product" method="POST">
-    <input type="text" name="title">
-    <button type="submit">Add Product</button>
-    </form>`);
-});
-
-app.use("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/')
-  
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello from Express</h1>");
-});
+app.use(amdinRoutes)
+app.use(shopRoutes)
 
 app.listen(3000, "localhost");
