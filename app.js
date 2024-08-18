7ca8ff1dcf3fe6e4f826371699107e9d85c72c99
mo99me99 <m.m.hosseini1099@gmail.com>
2024-08-18 21:25:53 +0330

    add views and rootDir

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const http = require("http");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const express = require("express");
+const path = require("path");
 
-const amdinRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const amdinRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:false}))
-app.use('/admin',amdinRoutes)
-app.use(shopRoutes)
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/admin", amdinRoutes);
+app.use(shopRoutes);
 
-app.use((req, res, next)=>{
-  res.status(404).send('<html><h1>404 : Page not found!</h1></html>')  
-})
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
 
 app.listen(3000, "localhost");
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,12 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
 
 const router = express.Router();
 
+const rootDir = require('../util/path');
 
 router.get("/", (req, res, next) => {
-  res.send("<h1>Hello from Express</h1>");
+  res.sendFile(path.join(__dirname, "..", "views", "shop.html"));
 });
 
-module.exports = router
+module.exports = router;
